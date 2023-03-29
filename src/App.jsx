b775a3b78809209233d184d3cf86cb42d93118d8
Victor Ribeiro <contato.victoribeiro@gmail.com>
2023-03-28 21:41:07 -0300

    Estruturando

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import Cards from './components/Cards/Cards';
+import Filtros from './components/Filtros/Filtros';
 
 function App() {
 
-
   return (
-    <div>
-      <h1>Hello, World! 🚀</h1>
+    <div className='App'>
+      <h1 className='text-center my-5 '>Víctor Ribeiro <span className='text-primary'>Teste</span></h1>
+
+
+        {/* Filtros */}
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filtros />
+          </div>
+          
+
+          {/* Cards */}
+          <div className="col-8"> 
+              <div className="row">
+                <Cards />
+                <Cards />
+                <Cards />
+              </div>            
+          </div>
+          
+        </div>
+      </div>
     </div>
   )
 }
